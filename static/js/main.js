0aca84e402f3be18b37ca87cdb42bf0c65a3d614
phongvt2004 <58397401+phongvt2004@users.noreply.github.com>
2022-01-31 15:42:12 +0700

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -216,9 +216,10 @@ function HappyNewYear(){
     videoJscii.play()
 }
 
-let lunarNewYear = new Date('Jan 31 2022 15:43:00:00 GMT+0700 (Indochina Time)')
-let afterLunarNewYear = new Date('Jan 31 2022 15:43:00:10 GMT+0700 (Indochina Time)')
+let lunarNewYear = new Date('Jan 31 2022 15:50:00:00 GMT+0700 (Indochina Time)')
+let afterLunarNewYear = new Date('Jan 31 2022 15:50:00:10 GMT+0700 (Indochina Time)')
 if (new Date() >= lunarNewYear) {
+	console.log('ok')	
     HappyNewYear()
 } else {
 	$('h2').text('COUNTDOWN')
